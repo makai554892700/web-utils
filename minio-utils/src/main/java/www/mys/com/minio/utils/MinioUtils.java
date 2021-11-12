cf7845eb4468e063f09ae5e7665fe4f8cd8f4f07
william <shengchen88888@gmail.com>
2021-11-12 10:25:38 +0800

    change:去除minio的log

--- minio-utils/src/main/java/www/mys/com/minio/utils/MinioUtils.java
+++ minio-utils/src/main/java/www/mys/com/minio/utils/MinioUtils.java
@@ -27,8 +27,8 @@ public class MinioUtils {
 
     public static boolean uploadFile(String host, String accessKey, String secretKey, String bucketName
             , MultipartFile file, String fileName) {
-        log.info("uploadFile host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
-                + ";bucketName=" + bucketName + ";fileName=" + fileName);
+//        log.info("uploadFile host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
+//                + ";bucketName=" + bucketName + ";fileName=" + fileName);
         try {
             MinioClient minioClient = new MinioClient(host, accessKey, secretKey);
             boolean isExist = minioClient.bucketExists(bucketName);
@@ -38,15 +38,15 @@ public class MinioUtils {
             minioClient.putObject(bucketName, fileName, file.getInputStream(), file.getContentType());
             return true;
         } catch (Exception e) {
-            log.info("uploadFile error: " + e);
+            log.info("uploadFile error fileName=" + fileName);
             return false;
         }
     }
 
     public static boolean uploadFile(String host, String accessKey, String secretKey, String bucketName
             , InputStream inputStream, String fileName, String contentType) {
-        log.info("uploadFile host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
-                + ";bucketName=" + bucketName + ";fileName=" + fileName + ";contentType=" + contentType);
+//        log.info("uploadFile host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
+//                + ";bucketName=" + bucketName + ";fileName=" + fileName + ";contentType=" + contentType);
         try {
             MinioClient minioClient = new MinioClient(host, accessKey, secretKey);
             boolean isExist = minioClient.bucketExists(bucketName);
@@ -56,50 +56,50 @@ public class MinioUtils {
             minioClient.putObject(bucketName, fileName, inputStream, contentType);
             return true;
         } catch (Exception e) {
-            log.info("uploadFile error: " + e);
+            log.info("uploadFile error fileName=" + fileName);
             return false;
         }
     }
 
     public static boolean downloadFile(HttpServletRequest request, HttpServletResponse response, String host
             , String accessKey, String secretKey, String bucketName, String objectName) {
-        log.info("downloadFile host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
-                + ";bucketName=" + bucketName + ";objectName=" + objectName);
+//        log.info("downloadFile host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
+//                + ";bucketName=" + bucketName + ";objectName=" + objectName);
         try {
             MinioClient minioClient = new MinioClient(host, accessKey, secretKey);
             boolean isExist = minioClient.bucketExists(bucketName);
             if (!isExist) {
-                log.info("bucketName not exist:bucketName=" + bucketName);
+                log.info("downloadFile bucketName not exist:bucketName=" + bucketName);
                 return false;
             }
             ObjectStat objectStat = minioClient.statObject(bucketName, objectName);
             return DownloadUtils.downloadFile(request, response, minioClient.getObject(bucketName, objectName)
                     , objectStat.name(), objectStat.length(), objectStat.contentType(), true, maxAge);
         } catch (Exception e) {
-            log.info("downloadFile error: " + e);
+            log.info("downloadFile error objectName=" + objectName);
         }
         return false;
     }
 
     public static void deleteFile(String host, String accessKey, String secretKey, String bucketName, String objectName) {
-        log.info("deleteFile host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
-                + ";bucketName=" + bucketName + ";objectName=" + objectName);
+//        log.info("deleteFile host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
+//                + ";bucketName=" + bucketName + ";objectName=" + objectName);
         try {
             MinioClient minioClient = new MinioClient(host, accessKey, secretKey);
             boolean isExist = minioClient.bucketExists(bucketName);
             if (!isExist) {
-                log.info("bucketName not exist:bucketName=" + bucketName);
+                log.info("deleteFile bucketName not exist:bucketName=" + bucketName);
                 return;
             }
             minioClient.removeObject(bucketName, objectName);
         } catch (Exception e) {
-            log.info("downloadFile error: " + e);
+            log.info("deleteFile error objectName=" + objectName);
         }
     }
 
     public static void deleteFiles(String host, String accessKey, String secretKey, String bucketName, List<String> objectNames) {
-        log.info("deleteFiles host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
-                + ";bucketName=" + bucketName + ";objectNames=" + objectNames);
+//        log.info("deleteFiles host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
+//                + ";bucketName=" + bucketName + ";objectNames=" + objectNames);
         try {
             MinioClient minioClient = new MinioClient(host, accessKey, secretKey);
             boolean isExist = minioClient.bucketExists(bucketName);
@@ -109,14 +109,14 @@ public class MinioUtils {
             }
             minioClient.removeObject(bucketName, objectNames);
         } catch (Exception e) {
-            log.info("downloadFile error: " + e);
+            log.info("deleteFile error objectName=" + objectNames);
         }
     }
 
     public static MYSFileInfo getFileInfo(String host, String accessKey, String secretKey, String bucketName
             , String objectName, boolean needStream) {
-        log.info("getFileInfo host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
-                + ";bucketName=" + bucketName + ";objectName=" + objectName);
+//        log.info("getFileInfo host=" + host + ";accessKey=" + accessKey + ";secretKey=" + secretKey
+//                + ";bucketName=" + bucketName + ";objectName=" + objectName);
         try {
             MinioClient minioClient = new MinioClient(host, accessKey, secretKey);
             boolean isExist = minioClient.bucketExists(bucketName);
@@ -168,7 +168,7 @@ public class MinioUtils {
                 }
             };
         } catch (Exception e) {
-            log.info("getFileInfo error.e=" + e);
+            log.info("getFileInfo error.objectName=" + objectName);
             return null;
         }
     }
